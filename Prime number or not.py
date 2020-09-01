2990e66727e623df88e809ec48562ba10f3f8bf3
Utkarsh Malik <32994258+Utkarshmalik99@users.noreply.github.com>
2020-09-01 21:33:04 +0530

    Create Prime number or not.py

---
+++ Prime number or not.py
@@ -0,0 +1,19 @@
+def isPrime(N):
+    #Your code here
+    if (N <= 1) : 
+        return False
+    if (N <= 3) : 
+        return True
+  
+    # This is checked so that we can skip  
+    # middle five numbers in below loop 
+    if (N % 2 == 0 or N % 3 == 0) : 
+        return False
+  
+    i = 5
+    while(i * i <= N) : 
+        if (N % i == 0 or N % (i + 2) == 0) : 
+            return False
+        i = i + 6
+  
+    return True
