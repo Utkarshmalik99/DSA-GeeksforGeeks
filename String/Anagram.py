ea65f818bdda6073f29c45d45db4ae78db389bf5
Utkarsh Malik <32994258+Utkarshmalik99@users.noreply.github.com>
2020-09-06 22:25:44 +0530

    Create Anagram.py

---
+++ String/Anagram.py
@@ -0,0 +1,36 @@
+def isAnagram(a,b):
+    if(sorted(a)==sorted(b)):
+        return True
+    else:
+        return False
+    #code here
+
+
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        a,b=map(str,input().strip().split())
+        if(isAnagram(a,b)):
+            print("YES")
+        else:
+            print("NO") 
+# } Driver Code Ends
