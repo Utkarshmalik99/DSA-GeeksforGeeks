89847479a6c1901783de57dff155c8d23d30f310
Utkarsh Malik <32994258+Utkarshmalik99@users.noreply.github.com>
2020-09-08 22:51:18 +0530

    Create Search an Element in an array.py

---
+++ Searching/Search an Element in an array.py
@@ -0,0 +1,29 @@
+def search(arr, N, X):
+    if X in arr:
+        return arr.index(X)
+    else:
+        return -1
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+
+import math    
+def main():
+        T=int(input())
+        while(T>0):
+            
+            N=int(input())
+            
+            A=[int(x) for x in input().strip().split()]
+            
+            x=int(input())
+            
+            print(search(A,N,x))
+            
+            T-=1
+
+
+if __name__ == "__main__":
+    main()
+# } Driver Code Ends
