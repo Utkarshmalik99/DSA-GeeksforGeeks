0e013af2fd34153ecfdd347be474fc97b3dd984b
Utkarsh Malik <32994258+Utkarshmalik99@users.noreply.github.com>
2020-09-09 12:41:24 +0530

    Create Binary To Gray Code.py

---
+++ Bit Magic/Binary To Gray Code.py
@@ -0,0 +1,23 @@
+def greyConverter(n):
+    return n ^ (n >> 1)
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+def main():
+    
+    T=int(input())
+    
+    while(T>0):
+        
+        
+        n=int(input())
+        print(greyConverter(n))
+        T-=1
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
