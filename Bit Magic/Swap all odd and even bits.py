45e747b07dfa61d9b8e3902390df9446d009f747
Utkarsh Malik <32994258+Utkarshmalik99@users.noreply.github.com>
2020-09-12 20:57:33 +0530

    Create Swap all odd and even bits.py

---
+++ Bit Magic/Swap all odd and even bits.py
@@ -0,0 +1,35 @@
+def swapBits(n):
+    even_bits = n & 0xAAAAAAAA
+
+    # Get all odd bits of x
+    odd_bits = n & 0x55555555
+
+    # Right shift even bits
+    even_bits >>= 1
+
+    # Left shift odd bits
+    odd_bits <<= 1
+
+    # Combine even and odd bits
+    return (even_bits | odd_bits)
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+def main():
+    
+    T=int(input())
+    
+    while(T>0):
+        
+        
+        n=int(input())
+        print(swapBits(n))
+        T-=1
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
