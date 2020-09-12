774d320b06efb0187cf7ceb636f3e92a89b690e2
Utkarsh Malik <32994258+Utkarshmalik99@users.noreply.github.com>
2020-09-12 21:02:19 +0530

    Check whether K-th bit is set or not

---
+++ Bit Magic/KthBit.py
@@ -0,0 +1,41 @@
+#{ 
+#Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+ # } Driver Code Ends
+
+def checkKthBit(n,k):
+    if (n & (1<<k)!=0):
+        return True
+    else:
+        return False
+
+
+#{ 
+#Driver Code Starts.
+
+
+    
+def main():
+    
+    T=int(input())
+    
+    while(T>0):
+        
+        
+        n=int(input())
+        k=int(input())
+        
+        if checkKthBit(n,k):
+            print("Yes")
+        else:
+            print("No")
+        
+        T-=1
+
+if __name__=="__main__":
+    main()
+#} Driver Code Ends
