55d193545fcc71abcfd2335ae6aa085358006e18
Utkarsh Malik <32994258+Utkarshmalik99@users.noreply.github.com>
2020-09-13 13:11:11 +0530

    Union of two arrays

---
+++ Hashing/Union.py
@@ -0,0 +1,20 @@
+def doUnion(a,n,b,m):
+    return len(set(a).union(set(b)))
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+#contributed by RavinderSinghPB
+if __name__=='__main__':
+    t=int(input())
+    for _ in range(t):
+        
+        n,m=[int(x) for x in input().strip().split()]
+        
+        a=[int(x) for x in input().strip().split()]
+        b=[int(x) for x in input().strip().split()]
+        
+        
+        print(doUnion(a,n,b,m))
+# } Driver Code Ends
